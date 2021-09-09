f50f798d9c7b359af4f603bd08fafc6d34245c6e
Dan <dan@aredherring.tech>
2021-09-09 21:28:10 +0100

    add empty module for podman api bindings

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,12 @@
-// https://podman.io/blogs/2020/06/29/podman-v2-announce.html
 #![cfg_attr(
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
+mod podman_api;
 
 fn main() {
+  podman_api::hello_world();
+
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
---
+++ src-tauri/src/podman_api/README.md
@@ -0,0 +1,2 @@
+Bindings to the podman A{O}
+See https://docs.podman.io/en/latest/_static/api.html
