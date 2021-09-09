e896c950c820158ac0ff4c0f9feea942ad108b02
Dan <dan@aredherring.tech>
2021-09-09 21:14:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+## Development Instructions
+
+1. Open tmux.
+2. Run `npm run parcel serve app/index.html` to start the development server for the web application.
+3. Open a new window.
+4. Run `npm run tauri dev` to start the Rust application.
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,11 @@
+// https://podman.io/blogs/2020/06/29/podman-v2-announce.html
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+fn main() {
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
